7be0939ede93421866be937b96b4479f6347c8f4
Sahil <n.sahil2709@gmail.com>
2026-03-23 09:29:54 +0530

    basic understanding of react component

---
+++ src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hello = () => {
+  return (
+    <div>
+      <h1>This is Sahil</h1>
+    </div>
+  )
+  // return React.createElement('div', null, React.createElement('h1', null, 'this is Sahil'))
+}
+
+export default Hello
